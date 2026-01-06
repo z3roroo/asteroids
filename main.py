81fb33e6a329369fc4e4d54d018c64bb020cc89f
z3roroo <andrew.rolf@rolfgroup.ca>
2026-01-06 11:53:05 -0700

    using groups

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
 
@@ -19,8 +24,10 @@ def main():
                 return
         
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+        for object in drawable:
+            object.draw(screen)
+
+        updatable.update(dt)
 
         pygame.display.flip()
 

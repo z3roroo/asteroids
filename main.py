40938f9e3e0811836c9b1b706259f0a65e5e2fcb
z3roroo <andrew.rolf@rolfgroup.ca>
2026-01-05 22:43:04 -0700

    draw player

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 import pygame # pyright: ignore[reportMissingImports]
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
 
     while True:
         log_state()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
-
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+        
+        
 if __name__ == "__main__":
     main()
---
+++ player.py
@@ -0,0 +1,20 @@
+
+import pygame # pyright: ignore[reportMissingImports]
+from circleshape import CircleShape
+from constants import PLAYER_RADIUS, LINE_WIDTH
+
+class Player(CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.rotation = 0  # in degrees
+
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+    
+    def draw(self, screen):
+        pygame.draw.polygon(screen, "white", self.triangle(), LINE_WIDTH)
